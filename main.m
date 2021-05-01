0076831d7faf9e29af293a871a4e21af6e8e2ae4
eastberlin <eastberlin@eastberlin-All-Series>
2021-05-01 21:15:10 +0200

    LastCommit

--- main.m
+++ main.m
@@ -1,2 +1,15 @@
 %% This is our main.m whicht will be used to execute the simulation!
 
+clc
+clear all
+
+SequenceLength = 1024;
+SeedPRBS = 1;
+ModulationOrder = 64;
+
+BitStream = nrPRBS(SeedPRBS, SequenceLength)';
+
+ComplexSymbols = Numerlogy.symbol_mapper(BitStream, ModulationOrder);
+
+fprintf("gray-coded-complex-symbolstream: \n");
+fprintf('%f%+fj\n', real(ComplexSymbols),imag(ComplexSymbols));
