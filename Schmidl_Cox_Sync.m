def4e3e9f796bafa4dbf55da5e3fca56f0472f51
Maximilian Klemm <s30299@beuth-hochschule.de>
2021-06-12 12:21:59 +0200

    added features to tresholder()

--- Schmidl_Cox_Sync.m
+++ Schmidl_Cox_Sync.m
@@ -16,8 +16,9 @@ classdef Schmidl_Cox_Sync
             obj.RxSignal    = [];
         end
     end
+    
     methods
-        function M_d = time_metric_creator(varargin)
+        function [M_d, R_d, P_d] = time_metric_creator(varargin)
             RxSequence = varargin{:}.RxSignal;
             denumerator = [1, -1];
             numerator = [1, zeros(1,varargin{:}.Size/2-2), -1];
@@ -32,21 +33,46 @@ classdef Schmidl_Cox_Sync
     end
     
     methods
+        function [PlateauLeftEdge, PlateauCenter, PlateauRightEdge...
+                , M_d] = thresholder(varargin)
+            [M_d, ~, ~] = time_metric_creator(varargin{:});
+            Threshold = 1.0;
+            [~,Index] = find(M_d >= Threshold);
+            PlateauStartIndex = Index(1);
+            PlateauEndIndex = Index(end);
+            PlateauCenterIndex = ceil((PlateauEndIndex - PlateauStartIndex)/2);
+            [PlateauLeftEdge,PlateauCenter, PlateauRightEdge] = deal(NaN(1,length(M_d)));       
+            [PlateauLeftEdge(PlateauStartIndex),...
+                PlateauRightEdge(PlateauEndIndex), ...
+                PlateauCenter(PlateauStartIndex + PlateauCenterIndex)] = deal(Threshold);
+        end
+    end
+    
+    methods
         function [TimeAxisNorm, SampleAxis] = show_metric(varargin)
-            M_d = time_metric_creator(varargin{:});
+            [PlateauLeftEdge, PlateauCenter, PlateauRightEdge...
+                , M_d] = thresholder(varargin{:});
             dt = (1/varargin{:}.TxInfo.SubcarrierSpacing)/...
                 varargin{:}.Size;
             TimeAxis = linspace(0,dt*(length(M_d)), length(M_d));
             TimeAxisNorm = TimeAxis/1e-6; % normalized to mircoseconds
             SampleAxis = linspace(0,length(M_d), length(M_d));
             
-            figure;
+            figure;            
             plot(SampleAxis, M_d), xlabel('Sample [Index]'), ylabel('M_d'),
             ax = gca;
             ax.XAxis.MinorTick = 'on';
             ax.XAxis.MinorTickValues = 0:200:SampleAxis(end);
-            ylim([0 1.2]), xlim([0 SampleAxis(end)])
+            ylim([0 1.2]), xlim([0 SampleAxis(end)]), xlim([0 2*varargin{:}.Size]),
+            title('S&C Timing Metric')
+            hold on,
+            stem(SampleAxis, PlateauLeftEdge,'.')
+            stem(SampleAxis, PlateauCenter,'.')
+            stem(SampleAxis, PlateauRightEdge,'.')
+            legend('Time Metric','Left Edge',...
+                'Centre AWGN Syncpoint', ...
+                'Right Edge Frame Syncpoint','Location', 'southwest')
+            hold off
         end
     end
 end
-  
